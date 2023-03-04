670c1029264176c50455fcd7326bad700707333d
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2023-03-04 22:34:28 +0530

    first commit

---
+++ config/dbConfig.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Contact_List');
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+    console.log(err.message);
+});
+
+//this event will be called only once, when the mongoose connection is made with the db
+db.once('open', () => {
+    console.log('MongoDB connected Successfully!');
+});
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const PORT = 8001;
+
+const db = require('./config/dbConfig');
+const Contact = require('./models/contact');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('assets'));
+
+
+app.get('/', async (req, res) => {                // Display all the contacts
+    try {
+        const contacts = await Contact.find({});
+        return res.render('home', {
+            title: 'Contact List',
+            contact_list: contacts
+        });
+    } catch (error) {
+        console.log("error in fetching data");
+    }
+});
+
+
+app.get('/practice', (req, res) => {
+    return res.render('practice', {
+        title: 'Playground'
+    })
+});
+
+
+app.post('/add-contact', async (req, res) => {     // Add contact to the Database
+    try {
+        const response = await Contact.create(req.body);
+        console.log(response);
+    } catch (error) {
+        console.log(error); 
+    }
+
+    return res.redirect('back');
+});
+
+
+app.get('/delete-contact/user', async (req, res) => {       // delete contact from database
+    try {
+        await Contact.findByIdAndRemove(req.query.id);
+        return res.redirect('back');
+    } catch (error) {
+        console.log("error in deleting contact")
+    } 
+})
+
+
+
+app.listen(PORT, async () => {
+    console.log(`Server started on PORT: ${PORT}`);
+    
+});
